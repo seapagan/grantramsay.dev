86bd88aaeee4eff2f123241eba3228fe83931d57
Grant Ramsay <grant@gnramsay.com>
2022-04-24 09:39:52 +0000

    Display the Projects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import ProjectList from "./components/ProjectList";
+import { settings } from "./configure/settings";
+
 import "./App.scss";
 
 function App() {
-  return <div className="app">GRANTRAMSAY.DEV</div>;
+  return (
+    <div className="app">
+      GRANTRAMSAY.DEV
+      <ProjectList projects={settings.projects} />
+    </div>
+  );
 }
 
 export default App;
