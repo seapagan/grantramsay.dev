c44f60e7b454cfd89ce0cde1c5bafdc69ca7446e
Grant Ramsay <grant@gnramsay.com>
2022-04-24 16:30:38 +0000

    add more projects data

--- src/configure/settings.js
+++ src/configure/settings.js
@@ -19,9 +19,10 @@ export const settings = {
       title: "Website : www.grantramsay.dev",
       summary:
         "React code for this website. Take a list of projects from a config " +
-        "file and list them on the page with links, image, summary etc.",
+        "file and list them on the page with links, image, summary etc. " +
+        "Responsive design.",
       github: "gnramsay/grantramsay.dev",
-      website: "",
+      website: "https://www.grantramsay.dev",
       tech: "React JS",
     },
     {
@@ -31,6 +32,7 @@ export const settings = {
         "A Django-based Blog engine with Comments, Tags, Likes, Pinned " +
         "Posts,  WYSIWYG Editing and more. Fully Responsive. Still a Work " +
         "in progress with many more features to add.",
+      image: "assets/images/django.png",
       github: "seapagan/myblog",
       website: "",
       tech: "Python, Django",
@@ -65,11 +67,26 @@ export const settings = {
     {
       name: "update_repo",
       title: "Update Repo",
-      summary: "A Ruby Gem to keep multiple cloned Git Repositories up to date",
-      image: "assets/images/project-placeholder.png",
+      summary:
+        "A Ruby Gem to keep multiple cloned Git Repositories up to " +
+        "date. The website code is in the repository and generated from data " +
+        "files using Webpack, soon to be re-written in React. About 50% of " +
+        "the application is covered by RSpec tests, working to improve that" +
+        "figure.",
       github: "seapagan/update_repo",
       website: "https://updaterepo.grantramsay.dev",
-      tech: "Ruby",
+      tech: "Ruby, RSpec",
+    },
+    {
+      name: "confoog",
+      title: "Confoog",
+      summary:
+        "A simple Gem to add a YAML configuration file to your Ruby script / " +
+        "Gem. This Gem is fully functional and I have been using it in" +
+        "several Ruby projects for years with no issues. The Gem is well" +
+        "documented on it's own website, the code for which is in the repo.",
+      github: "seapagan/confoog",
+      tech: "Ruby, Jekyll",
     },
     {
       name: "oot-get",
@@ -78,8 +95,7 @@ export const settings = {
         "Command-line Python package to download all the comic images for " +
         "the 'Order of the Stick' webcomic. Uses the 'Beautiful Soup' " +
         "package for web-scraping. Option to only check for new comics. " +
-        "This was an personal learning-exercise on Python Web-scraping.",
-      image: "assets/images/project-placeholder.png",
+        "This was a personal learning-exercise on Python Web-scraping.",
       github: "gnramsay/oots-get",
       website: "",
       tech: "Python",
@@ -90,7 +106,6 @@ export const settings = {
       summary:
         "Auto provision a new linux install with scripting tools and other " +
         "handy items inlcuding latest Python, Ruby, Node, Perl and more.",
-      image: "assets/images/project-placeholder.png",
       github: "seapagan/linux-comfy-chair",
       website: "",
       tech: "Shell script",
