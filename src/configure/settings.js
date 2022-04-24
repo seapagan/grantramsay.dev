503c4e503584dfd4ffcba32e13622fa3b360077d
Grant Ramsay <grant@gnramsay.com>
2022-04-24 09:39:06 +0000

    add some project data to work with

---
+++ src/configure/settings.js
@@ -0,0 +1,44 @@
+export const settings = {
+  projects: [
+    {
+      name: "update_repo",
+      title: "Update Repo",
+      summary: "summary for update_repo",
+      image: "assets/images/project-placeholder.png",
+      github: "seapagan/update_repo",
+      website: "https://updaterepo.grantramsay.dev",
+    },
+    {
+      name: "dev-portfolio-app",
+      title: "Developers Portfolio",
+      summary: "summary for dev-portfolio-app",
+      image: "assets/images/project-placeholder.png",
+      github: "seapagan/dev-portfolio-app",
+      website: "",
+    },
+    {
+      name: "vscode-create-python-module",
+      title: "VSCode Extension : Create Python Module",
+      summary: "summary for vscode-create-python-module",
+      image: "assets/images/project-placeholder.png",
+      github: "gnramsay/vscode-create-python-module",
+      website: "",
+    },
+    {
+      name: "oot-get",
+      title: "OOTS-Get",
+      summary: "summary for vscode-create-python-module",
+      image: "assets/images/project-placeholder.png",
+      github: "gnramsay/vscode-create-python-module",
+      website: "",
+    },
+    {
+      name: "linux-comfy-chair",
+      title: "Linux Comfy Chair",
+      summary: "summary for linux-comfy-chair",
+      image: "assets/images/project-placeholder.png",
+      github: "seapagan/linux-comfy-chair",
+      website: "",
+    },
+  ],
+};
